f6cc2b26f7b89f5f5fbcf190c967f58598ef8239
Swizec Teller <swizec@swizec.com>
2017-08-27 16:21:50 -0700

    remove annoying stuff

--- src/App.js
+++ src/App.js
@@ -12,12 +12,9 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h2>Welcome to React</h2>
-                </div>
+
                 <Scene artoolkit={{sourceType: 'webcam', trackingMethod: 'best'}}>
-                    <a-box position='0 0 0.5' material='opacity: 0.5;'></a-box>
+
                     <a-entity minecraft minecraft-head-anim="yes" minecraft-body-anim="hiwave" />
                     <a-marker-camera preset="hiro" />
                 </Scene>

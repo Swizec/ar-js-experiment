b8d90dfb8254d0d1efe2ac6775be1079e0891b43
Swizec Teller <swizec@swizec.com>
2017-08-27 16:16:34 -0700

    Trying to get a minecraft guy

--- src/App.js
+++ src/App.js
@@ -2,28 +2,28 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import 'aframe';
-import 'aframe-particle-system-component';
-import { Entity, Scene } from 'aframe-react';
+//import 'aframe';
+/* import 'aframe-particle-system-component';
+   import { Entity, Scene } from 'aframe-react'; */
+
+import { Scene, Entity } from 'aframe-react';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <Scene>
-            <Entity geometry={{primitive: 'box'}} material={{color: 'red'}} position={{x: 0, y: 0, z: -5}}/>
-            <Entity particle-system={{preset: 'snow'}}/>
-            <Entity light={{type: 'point'}}/>
-            <Entity gltf-model={{src: 'virtualcity.gltf'}}/>
-            <Entity text={{value: 'Hello, WebVR!'}}/>
-        </Scene>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h2>Welcome to React</h2>
+                </div>
+                <Scene artoolkit={{sourceType: 'webcam', trackingMethod: 'best'}}>
+                    <a-box position='0 0 0.5' material='opacity: 0.5;'></a-box>
+                    <a-entity minecraft minecraft-head-anim="yes" minecraft-body-anim="hiwave" />
+                    <a-marker-camera preset="hiro" />
+                </Scene>
+            </div>
+        );
+    }
 }
 
 export default App;

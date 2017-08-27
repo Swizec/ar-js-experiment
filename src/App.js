cf6385f36c5b5774d0662f917b44dc5ff08cf9b7
Swizec Teller <swizec@swizec.com>
2017-08-27 16:29:54 -0700

    Minecraft guy only shows up  in a box

--- src/App.js
+++ src/App.js
@@ -12,11 +12,13 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-
                 <Scene artoolkit={{sourceType: 'webcam', trackingMethod: 'best'}}>
 
-                    <a-entity minecraft minecraft-head-anim="yes" minecraft-body-anim="hiwave" />
-                    <a-marker-camera preset="hiro" />
+                    <a-anchor hit-testing-enabled="true">
+                        <a-entity minecraft minecraft-head-anim="yes" minecraft-body-anim="hiwave" material='opacity: 0.5' />
+                        <a-box position='0 0 0.5' material='opacity: 0.5;'></a-box>
+                    </a-anchor>
+                    <a-camera-static preset="hiro" />
                 </Scene>
             </div>
         );

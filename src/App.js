69f2a3f4d76c13a08686384ac6f22c4d6db93f66
Swizec Teller <swizec@swizec.com>
2017-09-03 19:38:21 -0700

    A flying missile

--- src/App.js
+++ src/App.js
@@ -8,21 +8,74 @@ import './App.css';
 
 import { Scene, Entity } from 'aframe-react';
 
+class BallisticMissile extends Component {
+
+    render() {
+        const { pos } = this.props;
+
+        return (
+            <Entity geometry={{primitive: 'box', width: .2, height: 1, depth: .2}}
+                    material={{roughness: 0, src: require('./texture.jpg')}}
+                    scale={{x: 1, y: 1, z: 1}}
+                    position={pos}
+
+                    />
+        )
+    }
+}
+
 class App extends Component {
+    state = {
+        missile: false,
+        launching: false
+    }
+
+    launch = () => {
+        this.setState({
+            missile: true,
+            launching: true,
+            missilePos: { x: 0, y: -4, z: 0 },
+            missileA: { ax: 0, ay: .005, az: 0 },
+            missileV: { vx: 0, vy: 0, vz: 0 }
+        });
+        if (!this.launched) {
+            window.requestAnimationFrame(() => this.gameLoop());
+            this.launched = true;
+        }
+    }
+
+    gameLoop() {
+        const { x, y, z } = this.state.missilePos,
+              { vx, vy, vz } = this.state.missileV,
+              { ax, ay, az } = this.state.missileA;
+
+        console.log(this.state.missilePos);
+
+        this.setState({
+            missilePos: { x: x+vx, y: y+vy, z: z+vz },
+            missileV: { vx: vx+ax, vy: vy+ay, vz: vz+az }
+        });
+        window.requestAnimationFrame(() => this.gameLoop());
+    }
+
     render() {
         const skinUrl = '/skins/jetienne.png';
+        const { missile, missilePos } = this.state;
 
         return (
             <div className="App">
                 <Scene artoolkit={{sourceType: 'webcam', trackingMethod: 'best'}}>
 
-                    <a-anchor hit-testing-enabled="true">
+                    <a-marker type="pattern">
                         <Entity minecraft={`heightMeter: 2; skinUrl: ${skinUrl}`}
                                 minecraft-head-anim="yes"
                                 minecraft-body-anim="hiwave" />
-                    </a-anchor>
-                    <a-camera-static preset="hiro" />
+                        {missile ? <BallisticMissile pos={missilePos} /> : null}
+                    </a-marker>
+                    <Entity camera />
                 </Scene>
+                <button onClick={() => this.launch()}
+                        style={{position: "absolute", zIndex: 10000, top: '0px'}}>Launch</button>
             </div>
         );
     }
